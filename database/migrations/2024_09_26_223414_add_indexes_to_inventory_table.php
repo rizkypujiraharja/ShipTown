b42b62b88005b05eed1eb2e5136d8871b6dd349f
Rizky Puji Raharja <rizky.rsj@gmail.com>
2024-09-27 05:36:08 +0700

    feat: add index to inventory table

---
+++ database/migrations/2024_09_26_223414_add_indexes_to_inventory_table.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::table('inventory', function (Blueprint $table) {
+            $table->index('total_cost');
+            $table->index('total_price');
+        });
+    }
+};
